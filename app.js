96cf8d5f847d95de080becdf20e94ec8fc0c74df
AhnafAhamed <ahnafahamed4@gmail.com>
2022-01-31 14:20:46 +0530

    fix: login issue

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(requestLogger);
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    email: Joi.string.required().email(),
+    email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
 }), createUser);
--- models/users.js
+++ models/users.js
@@ -1,4 +1,5 @@
 // const bcrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 const validator = require('validator');
 
@@ -22,5 +23,22 @@ const userSchema = new mongoose.Schema({
     select: false,
   },
 });
+
+userSchema.statics.findUserByCredentials = function findUserByCredentials(email, password) {
+  return this.findOne({ email })
+    .select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error('Invalid Credentials'));
+      }
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new Error('Invalid Credentials'));
+          }
+          return user;
+        });
+    });
+};
 
 module.exports = mongoose.model('user', userSchema);
