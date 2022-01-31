bc67575ad74aee5106d6766a5f2ee079e91e16f5
AhnafAhamed <ahnafahamed4@gmail.com>
2022-01-31 13:11:34 +0530

    feat: implement logger

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
 /* eslint-disable no-console */
 const express = require('express');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
+const cors = require('cors');
+const { errors } = require('celebrate');
 const { createArticle } = require('./controllers/articles');
 const { createUser, login } = require('./controllers/users');
 const users = require('./routes/users');
 const articles = require('./routes/articles');
+const auth = require('./middleware/auth');
+const { requestLogger, errorLogger } = require('./middleware/logger');
+const NotFoundError = require('./errors/not-found-error');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.use(express.json());
-
-app.post('/signup', createUser);
-app.post('/signin', login);
-app.post('/articles', createArticle);
-
-app.use('/', users);
-app.use('/', articles);
+require('dotenv').config();
 
 mongoose.connect('mongodb://localhost:27017/news-explorer', {
   useNewUrlParser: true,
@@ -25,6 +24,42 @@ mongoose.connect('mongodb://localhost:27017/news-explorer', {
   console.log('DB Connected');
 }).catch((err) => {
   console.log(err);
+});
+
+app.use(express.json());
+
+app.use(cors());
+app.options('*', cors());
+app.use(helmet());
+app.use(requestLogger);
+
+app.post('/signup', createUser);
+app.post('/signin', login);
+app.post('/articles', createArticle);
+
+app.use(auth);
+
+app.use('/', users);
+app.use('/', articles);
+
+app.get('*', () => {
+  throw new NotFoundError('Requested resource not found');
+});
+
+app.use(errorLogger);
+
+app.use(errors());
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+  res
+    .status(statusCode)
+    .send({
+      message: statusCode === 500
+        ? 'An error occurred on the server'
+        : message,
+    });
+  next();
 });
 
 app.listen(PORT, () => {
---
+++ middleware/auth.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+const AuthorizationError = require('../errors/auth-error');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
+
+const auth = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    throw new AuthorizationError('Authorization required');
+  }
+
+  const token = authorization.replace('Bearer ', '');
+
+  let payload;
+
+  try {
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
+  } catch (err) {
+    throw new AuthorizationError('Not Authorized');
+  }
+
+  req.user = payload;
+
+  return next();
+};
+
+module.exports = auth;
